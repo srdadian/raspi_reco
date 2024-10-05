a91f297b38cc7816c79678b46af2a141d3a1dc29
Stephane Dadian <stephane.dadian@gmail.com>
2024-10-05 14:47:42 +0200

    Initial commit

--- fort_condorcet/face_reco.py
+++ fort_condorcet/face_reco.py
@@ -1,2 +1,38 @@
+import numpy as np
+
+import picamera
 import face_recognition
 
+camera = picamera.PiCamera()
+camera.resolution = (320, 240)
+output = np.empty((240, 320, 3), dtype=np.uint8)
+
+# Load a sample picture and learn how to recognize it.
+print("Loading known face image(s)")
+obama_image = face_recognition.load_image_file("obama_small.jpg")
+obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
+
+# Initialize some variables
+face_locations = []
+face_encodings = []
+
+while True:
+    print("Capturing image.")
+    # Grab a single frame of video from the RPi camera as a numpy array
+    camera.capture(output, format="rgb")
+
+    # Find all the faces and face encodings in the current frame of video
+    face_locations = face_recognition.face_locations(output)
+    print("Found {} faces in image.".format(len(face_locations)))
+    face_encodings = face_recognition.face_encodings(output, face_locations)
+
+    # Loop over each face found in the frame to see if it's someone we know.
+    for face_encoding in face_encodings:
+        # See if the face is a match for the known face(s)
+        match = face_recognition.compare_faces([obama_face_encoding], face_encoding)
+        name = "<Unknown Person>"
+
+        if match[0]:
+            name = "Barack Obama"
+
+        print("I see someone named {}!".format(name))
--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import picamera
 from fort_condorcet.camera_server import StreamingServer, StreamingHandler, StreamingOutput
 
 with picamera.PiCamera(resolution='1920x1080', framerate=24) as camera:
-    output = StreamingOutput()
-    # Uncomment the next line to change your Pi's Camera rotation (in degrees)
+    camera_stream = StreamingOutput()
     # camera.rotation = 90
-    camera.start_recording(output, format='mjpeg')
+    camera.start_recording(camera_stream, format='mjpeg')
+
+
     try:
         address = ('', 8000)
         server = StreamingServer(address, StreamingHandler)
